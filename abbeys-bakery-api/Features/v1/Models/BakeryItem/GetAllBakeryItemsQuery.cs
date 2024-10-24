837fa1a9ac2040624fef72116ed87ca071c32013
adinc <adinhiebschcharles@gmail.com>
2024-10-23 20:53:39 -0500

    Added a model for retrieving all of the bakery items

---
+++ abbeys-bakery-api/Features/v1/Models/BakeryItem/GetAllBakeryItemsQuery.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+
+namespace abbeys_bakery_api.Features.v1.Models.BakeryItem
+{
+    public class GetAllBakeryItemsQuery
+    {
+        public class GetAllBakeryItemsRequest : IRequest<GetAllBakeryItemsResponse>
+        {
+
+        }
+
+        public class GetAllBakeryItemsResponse
+        {
+            public List<BakeryItem> BakeryItems { get; set; }
+        }
+
+        public class BakeryItem
+        {
+
+        }
+
+        public class Handler : IRequestHandler<GetAllBakeryItemsRequest, GetAllBakeryItemsResponse>
+    }
+}
