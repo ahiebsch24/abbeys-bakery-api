e69a10a103fc87aecbea4c58bb5be4f2fe1f4825
adinc <adinhiebschcharles@gmail.com>
2025-01-26 22:26:19 -0600

    added infrastructur for main menu and cart

---
+++ abbeys-bakery-api/Features/v1/Controllers/CartController.cs
@@ -0,0 +1,49 @@
+﻿using abbeys_bakery_api.Features.v1.Models.Cart;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace abbeys_bakery_api.Features.v1.Controllers
+{
+    [Route("Cart")]
+    public class CartController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public CartController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [Route("addCartItem")]
+        [HttpPost]
+        public async Task<ActionResult> AddItemToCart([FromBody] AddItemToCart.AddItemToCartRequest request)
+        {
+            _mediator.Send(request);
+            return Ok();
+        }
+
+        [Route("adjustCartItemQuantity")]
+        [HttpPatch]
+        public async Task<ActionResult> IncreaseCartQuantity([FromQuery] AdjustCartItemQuantity.AdjustCartItemQuantityRequest request)
+        {
+            _mediator.Send(request);
+            return Ok();
+        }
+
+        [Route("getAllCartItemsForASpecificUser")]
+        [HttpGet]
+        public async Task<ActionResult<GetAllCartItemsForASpecificUser.GetAllCartItemsForASpecificUserRequest>> GetAllCartItemsForASpecificUserRequest([FromQuery] GetAllCartItemsForASpecificUser.GetAllCartItemsForASpecificUserRequest request)
+        {
+            var response = await _mediator.Send(request);
+            return Ok(response);
+        }
+
+        [Route("deleteItemFromCart")]
+        [HttpDelete]
+        public async Task<ActionResult> DeleteItemFromCart([FromQuery] DeleteItemFromCart.DeleteItemFromCartRequest request)
+        {
+            await _mediator.Send(request);
+            return Ok();
+        }
+    }
+}
---
+++ abbeys-bakery-api/Features/v1/Models/Cart/GetAllCartItemsForASpecifcUser.cs
@@ -0,0 +1,52 @@
+﻿using abbeys_bakery_api.Entities;
+using AutoMapper;
+using MediatR;
+
+namespace abbeys_bakery_api.Features.v1.Models.Cart
+{
+    public class GetAllCartItemsForASpecificUser
+    {
+        public class GetAllCartItemsForASpecificUserRequest : IRequest<GetAllCartItemsForASpecificUserResponse>
+        {
+            public string UniqueUserId { get; set;}
+        }
+
+        public class GetAllCartItemsForASpecificUserResponse
+        {
+            public List<UserCartItem> Items { get; set;} = new List<UserCartItem>();
+        }
+
+        public class UserCartItem
+        {
+            public string ItemDescription { get; set;}
+            public string Title { get; set;}
+            public decimal Price { get; set;}
+            public int Quantity { get; set;}
+        }
+
+        public class Handler : IRequestHandler<GetAllCartItemsForASpecificUserRequest, GetAllCartItemsForASpecificUserResponse>
+        {
+            private AbbeysBakeryContext _abbeysBakeryContext;
+            private readonly IMapper _mapper;
+
+            public Handler(AbbeysBakeryContext abbeysBakeryContext, IMapper mapper)
+            {
+                _abbeysBakeryContext = abbeysBakeryContext;
+                _mapper = mapper;
+            }   
+
+            public async Task<GetAllCartItemsForASpecificUserResponse> Handle(GetAllCartItemsForASpecificUserRequest request, CancellationToken cancellationToken)
+            {
+                Guid uniqueUserId = new Guid(request.UniqueUserId);
+                GetAllCartItemsForASpecificUserResponse response = new GetAllCartItemsForASpecificUserResponse();
+                var cartItems = this._abbeysBakeryContext.CartItems.Where(x => x.UniqueUserId == uniqueUserId).ToList();
+                foreach ( var cartItem in cartItems )
+                {
+                    UserCartItem item = this._mapper.Map<UserCartItem>(cartItem);
+                    response.Items.Add(item);
+                }
+                return response;
+            }
+        }
+    }
+}
